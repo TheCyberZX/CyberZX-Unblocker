46c64be6559ca9164e593ce6ab5118d95277ee9a
TheCyberZX <cyber_zx@outlook.com>
2025-04-08 02:11:31 -0500

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -174,16 +174,10 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
+  "Made by CyberZX Team",
   "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
+  "Subscribe to us on YouTube (@CyberZX-zx)",
   "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
